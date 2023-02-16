3ee1a6d7c366a6d9c2ed5d9adde51bbf43974f63
Sophie Strausberg <sophie.strausberg@gmail.com>
2023-02-15 20:24:20 -0500

    Initial Commit

---
+++ MusicTherapyApp/MusicTherapyAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MusicTherapyAppApp.swift
+//  MusicTherapyApp
+//
+//  Created by Sophie Strausberg on 2/15/23.
+//
+
+import SwiftUI
+
+@main
+struct MusicTherapyAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
