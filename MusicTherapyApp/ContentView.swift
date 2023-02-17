0c7cf05675016f389aa498e6e8b40cd967f47456
Sophie Strausberg <sophie.strausberg@gmail.com>
2023-02-17 10:37:38 -0500

    add more text

--- MusicTherapyApp/ContentView.swift
+++ MusicTherapyApp/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundColor(.accentColor)
-            Text("Hello, world! This is my app.")
+            Text("Hello, world! This is my app. More text.")
         }
         .padding()
     }
